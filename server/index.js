45a6e0d166f9b176002028cb983b8463c22e815b
SYNTAX <kavinduchamath02@gmail.com>
2024-08-11 13:54:14 +0530

    add static files serve

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import "dotenv/config";
+import path from "path";
 
 // Initialize Server with WebSocket
 import { app, server } from "./utilities/webSocket.js";
@@ -21,8 +22,16 @@ import messageRoutes from "./routes/message.routes.js";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "http://localhost:5000", credentials: true }));
+
+// Server Port
 const port = process.env.PORT || 5000;
+
+// __dirname
+const __dirname = path.resolve();
+
+// Serve static files from the public folder
+app.use(express.static(path.join(__dirname, "client/dist")));
 
 // Auth Routes
 app.use("/api/auth", authRoutes);
@@ -32,6 +41,11 @@ app.use("/api/conversation", authMiddleware, conversationRoutes);
 
 // Message Routes
 app.use("/api/message", authMiddleware, messageRoutes);
+
+// Send all requests to the React app
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/dist/index.html"));
+});
 
 server.listen(port, () => {
     connectDatabase();
